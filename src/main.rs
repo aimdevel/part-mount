7bfc3016d9d87278131f33c766ab5f15118b64b1
aimdevel <hidemasaaim+devel@gmail.com>
2023-08-05 15:23:50 +0900

    first commit.

---
+++ src/main.rs
@@ -0,0 +1,92 @@
+extern crate libparted;
+extern crate sys_mount;
+
+use std::process::exit;
+use clap::Parser;
+use libparted::{Device, Disk};
+use sys_mount::{
+    Mount,
+};
+
+#[derive(Parser)]
+struct Args {
+    /// Target device name.
+    device: String,
+    /// Partition number
+    #[arg(short)]
+    partition_number: i32,
+    /// Mount point
+    mountdir: String,
+}
+
+struct PartMount{
+    device: String,
+    mount_point: String,
+    partition_number: i32,
+}
+
+impl PartMount{
+    fn get_offset(&self) -> Option<u64> {
+        let mut dev = match Device::get(&self.device){
+            Ok(dev) => dev,
+            Err(why) => {
+                eprintln!("unable to get {} device: {}", &self.device,why);
+                return None;
+            }
+        };
+        let sector_size = dev.sector_size();
+        let disk = match Disk::new(&mut dev){
+            Ok(disk) => disk,
+            Err(why) => {
+                eprintln!("unable to create disk object: {}", why);
+                return None;
+            }
+        };
+        for part in disk.parts() {
+            if part.num() == self.partition_number{
+                let offset = part.geom_start() as u64 * sector_size;
+                return Some(offset);
+            }
+        }
+        eprintln!("partition {} is not found.", self.partition_number);
+        return None;
+    }
+
+    pub fn mount(&self) {
+        let offset = match self.get_offset() {
+            Some(x) => x,
+            None => {
+                eprintln!("cannot get partition's offset");
+                0
+            },
+        };
+        if offset == 0 {
+            return;
+        }
+        let mount_builder = Mount::builder()
+            .loopback_offset(offset);
+
+        let mount_result = mount_builder.mount(&self.device, &self.mount_point);
+
+        match mount_result {
+            Ok(_) => eprintln!("mount success!"),
+            Err(why) => eprintln!("error!:{}", why),
+        };
+    }
+}
+
+fn main(){
+    let args = Args::parse();
+
+    if args.partition_number < 1 {
+        eprintln!("invalid partition number:{}.", args.partition_number);
+        exit(1);
+    }
+
+    let mount = PartMount{
+        device: args.device,
+        mount_point: args.mountdir,
+        partition_number: args.partition_number,
+    };
+    mount.mount();
+}
